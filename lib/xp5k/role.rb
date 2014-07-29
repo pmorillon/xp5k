151f1bcfc90444e377462d0f55ab4a53c3ef2074
Pascal Morillon <pascal.morillon@irisa.fr>
2014-07-29 22:39:11 +0200

    Add role in subset of role feature #8

--- lib/xp5k/role.rb
+++ lib/xp5k/role.rb
@@ -1,11 +1,16 @@
 class XP5K::Role
 
-  attr_accessor :name, :size, :desc, :servers, :jobid
+  attr_accessor :name, :size, :desc, :servers, :jobid, :inner
+
+  @@roles = []
 
   def initialize(options = {})
+    # Defaults
+    @inner   = false
+    @servers = []
+    @desc    = ""
+
     # Required parameters
-    @name = ""
-    @size = 0
     %w{ name size }.each do |param|
       if options[param.to_sym].nil?
         raise XP5K::Exceptions::Role, "#{self.to_s}: #{param.to_sym} needed at initialization"
@@ -15,28 +20,62 @@ class XP5K::Role
     end
 
     # Optional parameters
-    %w{ desc servers }.each do |param|
-      instance_variable_set("@#{param}", options[param.to_sym])
+    %w{ desc servers inner }.each do |param|
+      instance_variable_set("@#{param}", options[param.to_sym]) if options[param.to_sym]
     end
   end
 
   def self.create_roles(job, job_definition)
-    count_needed_nodes = 0
+    # Definition will return list of roles
     roles = []
-    job_definition[:roles].each { |role| count_needed_nodes += role.size }
+
+    # Test if job contain enough nodes for all roles
+    count_needed_nodes = 0
+    job_definition[:roles].each { |role| count_needed_nodes += role.size if not role.inner }
     if job['assigned_nodes'].length < count_needed_nodes
-      self.clean
       raise "Job ##{job['uid']} require more nodes for required roles"
     end
-    available_nodes = job['assigned_nodes'].sort
-    job_definition[:roles].each do |role|
-      role.servers = available_nodes[0..(role.size - 1)]
-      available_nodes -= role.servers
+
+    # Sort nodes assigned to the job
+    available_nodes = { 'job' => job['assigned_nodes'].sort }
+
+
+    # Sort roles to manage inner roles at the end
+    defined_roles = job_definition[:roles].sort do |x,y|
+      a = x.inner ? 1 : 0
+      b = y.inner ? 1 : 0
+      a <=> b
+    end
+
+    # Attributes nodes to roles
+    defined_roles.each do |role|
+      next if self.exists?(role.name)
+      if role.inner
+        available_nodes[role.inner] ||= self.findByName(role.inner).servers
+        role.servers = available_nodes[role.inner][0..(role.size - 1)]
+        available_nodes[role.inner] -= role.servers
+      else
+        role.servers = available_nodes['job'][0..(role.size - 1)]
+        available_nodes['job'] -= role.servers
+      end
       role.jobid = job['uid']
-      next if not roles.select { |x| x.name == role.name }.empty?
       roles << role
+      @@roles << role
     end
     return roles
+  end
+
+  def self.list()
+    @@roles
+  end
+
+  def self.findByName(name)
+    roles = @@roles.select { |x| x.name == name }
+    roles.empty? ? nil : roles.first
+  end
+
+  def self.exists?(name)
+    @@roles.select { |x| x.name == name }.empty? ? false : true
   end
 
 end
